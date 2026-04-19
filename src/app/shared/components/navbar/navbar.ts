de65b554adbe1be8b44eb249199ec514bf0e0ef2
Neagu Robert Alexandru <127115019+NeaguRobertAlexandru31@users.noreply.github.com>
2026-04-19 12:01:28 +0200

    @new fonts and navbar

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { TranslatePipe } from '../../../i18n/translate.pipe';
+
+export interface NavItem {
+  route: string;
+  labelKey: string;
+}
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, TranslatePipe],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  readonly navItems: NavItem[] = [
+    { route: '/about-us',  labelKey: 'nav.about-us'  },
+    { route: '/donations', labelKey: 'nav.donations'  },
+    { route: '/events',    labelKey: 'nav.events'     },
+    { route: '/news',      labelKey: 'nav.news'       },
+    { route: '/projects',  labelKey: 'nav.projects'   },
+  ];
+}
